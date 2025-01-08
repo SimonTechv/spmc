bedfd14424a3b3a50c896ee9333b16309b2ec2ae
unknown <simh0xff@gmail.com>
2025-01-08 19:26:06 +0300

    MVP build

---
+++ core/main.c
@@ -0,0 +1,40 @@
+/***************************************************************************//**
+ * @file main.c
+ * @brief main() function.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
+#include "stdint.h"
+
+#include "em_device.h"
+#include "em_cmu.h"
+#include "em_emu.h"
+#include "em_gpio.h"
+#include "em_chip.h"
+
+int main(void)
+{
+
+  CHIP_Init();
+
+  /* Enable GPIO peripheral unit */
+  CMU_ClockEnable(cmuClock_GPIO, true);
+
+  GPIO_PinModeSet(gpioPortC, 13, gpioModePushPull, 0);
+  GPIO_PinOutSet(gpioPortC, 13);
+
+  for(;;)
+  {
+
+  }
+}
---
+++ sources.cmake
@@ -0,0 +1,40 @@
+# Setting SDK sources
+set(EFM32SDKSRC
+    # System scripts
+    sdk/platform/common/src/sl_assert.c
+    sdk/platform/common/src/sl_syscalls.c
+    sdk/platform/common/toolchain/src/sl_memory.c
+
+    # Startup scripts
+    sdk/platform/Device/SiliconLabs/EFM32ZG/Source/startup_efm32zg.c
+    sdk/platform/Device/SiliconLabs/EFM32ZG/Source/system_efm32zg.c
+
+    # EmLib sources
+    sdk/platform/emlib/src/em_cmu.c
+    sdk/platform/emlib/src/em_core.c
+    sdk/platform/emlib/src/em_emu.c
+    sdk/platform/emlib/src/em_gpio.c
+    sdk/platform/emlib/src/em_msc.c
+    sdk/platform/emlib/src/em_rtc.c
+    sdk/platform/emlib/src/em_system.c
+    sdk/platform/emlib/src/em_timer.c
+)
+
+# Setting SDK includes
+set(EFM32SDKINC
+    sdk/platform/CMSIS/core/include
+    sdk/platform/common/inc
+    sdk/platform/common/toolchain/inc
+    sdk/platform/Device/SiliconLabs/EFM32ZG/Include
+    sdk/platform/emlib/inc
+)
+
+# Core application sources
+set(EFM32CORESRC
+    core/main.c
+)
+
+# Core application include
+set(EFM32COREINC
+    core/
+)
